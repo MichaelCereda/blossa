233f57ecb091352044784aa8faba55a7af92de39
Michael Cereda <107390+MichaelCereda@users.noreply.github.com>
2020-12-24 17:24:01 -0800

    Expose BlossaRoute

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Blossa, BlossaMiddlewareContext } from "./blossa";
 import { BlossaResponse } from "./internals/response";
-
+import { BlossaRoute } from "./internals/route";
 import {Handler} from './internals/router';
 
 export default Blossa;
 
 export {
+    BlossaRoute,
     BlossaResponse, 
     Handler as BlossaHandler,
     BlossaMiddlewareContext
